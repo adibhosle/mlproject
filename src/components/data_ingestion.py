f0e6cb18bf93397acede0b58454d9e7a9f97fae1
adibhosle <adityabhosle900@gmail.com>
2024-09-14 10:48:09 +0530

    Data Ingestion added

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,50 @@
+import os
+import sys 
+import pandas as pd
+
+# parent_directory = os.path.abspath('.')
+# print(parent_directory)
+sys.path.append(os.path.abspath('.'))
+from src.exception import CustomException
+from src.logger import logging
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+
+@dataclass
+class DataIngestionConfig():
+    raw_data_path: str =  os.path.join('artifacts', "data.csv")
+    train_data_path: str =  os.path.join('artifacts', "train.csv")
+    test_data_path: str =  os.path.join('artifacts', "test.csv")
+
+class DataIngestion():
+    def __init__(self):
+        self.ingestionConfig = DataIngestionConfig()
+
+    def initial_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df = pd.read_csv('notebook/data/stud.csv')
+            logging.info("Read the dataset as dataframe")
+
+            os.makedirs(os.path.dirname(self.ingestionConfig.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestionConfig.raw_data_path, index=False, header=True)
+            logging.info("Train & Test split initiated")
+
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=40)
+            train_set.to_csv(self.ingestionConfig.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestionConfig.test_data_path, index=False, header=True)
+            logging.info("Data ingetsion completed......")
+
+            return (self.ingestionConfig.train_data_path, self.ingestionConfig.test_data_path)
+
+        except Exception as e:
+            # pass
+            raise CustomException(e, sys)
+        
+
+if __name__ == '__main__':
+    obj = DataIngestion()
+    obj.initial_data_ingestion()
